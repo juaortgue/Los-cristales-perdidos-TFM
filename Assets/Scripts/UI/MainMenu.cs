4b2715fca852721ed0cc59da03b87b3f08d65827
juaortgue <juanantonioortizguerra@gmail.com>
2025-04-27 17:30:32 +0200

    added controls menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,22 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     public void LoadFirstScene(){
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
+    
     public void ExitGame(){
         Application.Quit();
     }
+
+    public void LoadControlsScene(){
+        SceneManager.LoadScene(1);
+    }
+    public void LoadMainMenuScene(){
+        SceneManager.LoadScene(0);
+    }
+    
 }
